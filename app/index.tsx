299da080b57b86ea84d10482957bca4ce989b8ca
Santiago Bancalari <sbancalari@midasconsultores.com.ar>
2025-03-25 13:58:26 -0300

    [CONFIG](nativewind): Added nativewind configuration

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HOME</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl">HOME</Text>
     </View>
   );
 };
